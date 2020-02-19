351996f5fd638fbdf9df46605753f4b0ed065723
Ri-Dearg <sheridan.rp@gmail.com>
2020-02-19 21:22:52 +0000

    Added dialogues to newsletter slip

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,4 +1,4 @@
-const contactF = document.getElementById("contactform");
+const contactF = document.getElementById("emailform");
 
 contactF.addEventListener('submit', submitAct);
 
@@ -16,28 +16,41 @@ function clearInput() {
 
 // Posts thank you message into modal
 function okResponse() {
-    $('#contactheader').html(`Thank You`);
-    $('#contactbody').html(`Your email has been sent. You will receive a response shortly.`);
-    $('#contactmodal').modal('show');
+    $('#emailmodalheader').html(`Thank You`);
+    $('#emailmodalbody').html(`You will receive a response shortly.`);
+    $('#emailmodal').modal('show');
     clearInput();
 }
 
 // Posts failure message into modal
 function errorResponse(error) {
-    $('#contactheader').html(`Sorry!`);
-    $('#contactbody').html(`There appears to be a problem sending your email. <br>
+    $('#emailmodalheader').html(`Sorry!`);
+    $('#emailmodalbody').html(`There appears to be a problem! <br>
                             Error Info: <br>
                             ${error.status}${error.text}`);
-    $('#contactmodal').modal('show');;
+    $('#emailmodal').modal('show');;
 }
 
 // Sends email with all parameters
-function sendMail(contactForm) {
+function sendMailContact(contactForm) {
     emailjs.send('horizon_photo', 'basic', {
         'user_name': contactForm.name.value,
         'user_email': contactForm.email.value,
         'user_number': contactForm.number.value,
         'user_message': contactForm.message.value,
+    })
+        .then(function (response) {
+            okResponse();
+            console.log('SUCCESS!', response.status, response.text);
+        }, function (error) {
+            console.log('FAILED...', error);
+            errorResponse(error);
+        });
+}
+
+function sendMailNews(contactForm) {
+    emailjs.send('horizon_photo', 'basic', {
+        'user_email': contactForm.email.value,
     })
         .then(function (response) {
             okResponse();
